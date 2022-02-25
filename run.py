e241ef9462a0b2e205c6ff682da0683c817cf162
dbsgmlrua <dbsgmlrua@naver.com>
2022-02-25 19:59:40 +0900

    metadata.json to  .csv file

--- run.py
+++ run.py
@@ -1,5 +1,25 @@
+import json
+import pandas as pd
+
 def makeExcel(data):
-    print(data)
+    result = []
+    for d in data:
+        dic = {}
+        dic['name'] = d['name']
+        attribute = d['attributes']
+        
+        for a in attribute:
+            dic[a['trait_type']] = a['value']
+        result.append(dic)
+    
+    return result
     
 if __name__ == "__main__":
-    makeExcel("run")
+    f = open('_metadata.json')
+    jsondata = json.load(f)
+    result = makeExcel(jsondata)
+    df = pd.DataFrame(result)
+    print(df)
+
+    df.to_csv('result.csv', index = False, header=True)
+
